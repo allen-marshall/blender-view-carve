53fc7147316aaa845c7503c1eb6e25742b622428
Allen Marshall <allen.marshall.22@gmail.com>
2019-03-12 20:57:28 -0500

    Commented out some code that shouldn't be used yet.

--- src/view_carve/mesh_project.py
+++ src/view_carve/mesh_project.py
@@ -102,79 +102,81 @@ def _carver_mesh_to_stencil_shape(to_vp_matrix, carver_mesh):
     to_vp_matrix - Transformation matrix from the mesh object's local 3D space to the viewport's 3D space.
     carver_mesh - A Blender mesh indicating the carver geometry to use.
     """
-    return _faceless_bmesh_to_stencil_shape(to_vp_matrix, mesh) if len(mesh.faces) <= 0 \
-        else _faced_bmesh_to_stencil_shape(to_vp_matrix, mesh)
+    # TODO: Fix this function.
+    # return _faceless_bmesh_to_stencil_shape(to_vp_matrix, mesh) if len(mesh.faces) <= 0 \
+    #     else _faced_bmesh_to_stencil_shape(to_vp_matrix, mesh)
+    pass
 
 
-def _faced_bmesh_to_stencil_shape(to_vp_matrix, mesh):
-    # Convert individual faces into stencil shapes.
-    def face_to_stencil_shape(face):
-        vert_coords = [_vp_plane_project_pt(to_vp_matrix, vert.co) for vert in face.verts]
-        return Polygon(vert_coords)
-    face_stencil_shapes = [face_to_stencil_shape(face) for face in mesh.faces]
-
-    # Filter out any invalid face shapes. (These can arise e.g. from non-planar faces.)
-    face_stencil_shapes = [shape for shape in face_stencil_shapes if shape.is_valid()]
-
-    # Union the face shapes together.
-    stencil_shape = shapely.ops.unary_union(face_stencil_shapes)
-
-    return stencil_shape if stencil_shape.is_valid() else None
-
-
-def _faceless_bmesh_to_stencil_shape(to_vp_matrix, mesh):
-    # Try to extract an open or closed path from the mesh by following edges starting from an arbitrary vertex.
-    if len(list(mesh.verts)) <= 0:
-        return None
-    start_vert = mesh.verts[0]
-    start_edges = start_vert.link_edges()
-    start_edges.index_update()
-    if len(start_edges) not in {1, 2}:
-        return None
-    (looped, pts_before) = _follow_edges(start_vert, start_edges[0])
-    pts_after = [] if looped else (_follow_edges(start_vert, start_edges[1])[1] if len(start_edges) > 1 else [])
-    if pts_before is None or pts_after is None:
-        return None
-
-    # If the path does not include all vertices, the mesh is not path-shaped.
-    if len(pts_before) + len(pts_after) + 1 != len(mesh.verts):
-        return None
-
-    pts_before.reverse()
-    pts = [_vp_plane_project_pt(to_vp_matrix, pt) for pt in pts_before + [start_vert.co] + pts_after]
-
-    # Try to convert the path to a 2D shape in the viewport, closing the path if it is open. This can fail, e.g. due to
-    # a self-intersecting path.
-    stencil_shape = Polygon(pts)
-
-    return stencil_shape if stencil_shape.is_valid() else None
-
-
-def _follow_edges(start_vert, start_edge):
-    pts = []
-    prev_vert = start_vert
-    curr_vert = start_edge.other_vert(start_vert)
-    curr_edges = curr_vert.link_edges
-    curr_edges.index_update()
-    while len(curr_edges) > 1 and curr_vert != start_vert:
-        if len(curr_edges) != 2:
-            return False, None
-        else:
-            pts.append(curr_vert.co)
-
-            other_vert0 = curr_edges[0].other_vert(curr_vert)
-            other_vert1 = curr_edges[1].other_vert(curr_vert)
-            next_vert = other_vert0 if other_vert0 != prev_vert else other_vert1
-
-            prev_vert = curr_vert
-            curr_vert = next_vert
-            curr_edges = curr_vert.link_edges
-
-    if curr_vert != start_vert:
-        pts.append(curr_vert.co)
-        return False, pts
-    else:
-        return True, pts
+# def _faced_bmesh_to_stencil_shape(to_vp_matrix, mesh):
+#     # Convert individual faces into stencil shapes.
+#     def face_to_stencil_shape(face):
+#         vert_coords = [_vp_plane_project_pt(to_vp_matrix, vert.co) for vert in face.verts]
+#         return Polygon(vert_coords)
+#     face_stencil_shapes = [face_to_stencil_shape(face) for face in mesh.faces]
+#
+#     # Filter out any invalid face shapes. (These can arise e.g. from non-planar faces.)
+#     face_stencil_shapes = [shape for shape in face_stencil_shapes if shape.is_valid()]
+#
+#     # Union the face shapes together.
+#     stencil_shape = shapely.ops.unary_union(face_stencil_shapes)
+#
+#     return stencil_shape if stencil_shape.is_valid() else None
+#
+#
+# def _faceless_bmesh_to_stencil_shape(to_vp_matrix, mesh):
+#     # Try to extract an open or closed path from the mesh by following edges starting from an arbitrary vertex.
+#     if len(list(mesh.verts)) <= 0:
+#         return None
+#     start_vert = mesh.verts[0]
+#     start_edges = start_vert.link_edges()
+#     start_edges.index_update()
+#     if len(start_edges) not in {1, 2}:
+#         return None
+#     (looped, pts_before) = _follow_edges(start_vert, start_edges[0])
+#     pts_after = [] if looped else (_follow_edges(start_vert, start_edges[1])[1] if len(start_edges) > 1 else [])
+#     if pts_before is None or pts_after is None:
+#         return None
+#
+#     # If the path does not include all vertices, the mesh is not path-shaped.
+#     if len(pts_before) + len(pts_after) + 1 != len(mesh.verts):
+#         return None
+#
+#     pts_before.reverse()
+#     pts = [_vp_plane_project_pt(to_vp_matrix, pt) for pt in pts_before + [start_vert.co] + pts_after]
+#
+#     # Try to convert the path to a 2D shape in the viewport, closing the path if it is open. This can fail, e.g. due to
+#     # a self-intersecting path.
+#     stencil_shape = Polygon(pts)
+#
+#     return stencil_shape if stencil_shape.is_valid() else None
+#
+#
+# def _follow_edges(start_vert, start_edge):
+#     pts = []
+#     prev_vert = start_vert
+#     curr_vert = start_edge.other_vert(start_vert)
+#     curr_edges = curr_vert.link_edges
+#     curr_edges.index_update()
+#     while len(curr_edges) > 1 and curr_vert != start_vert:
+#         if len(curr_edges) != 2:
+#             return False, None
+#         else:
+#             pts.append(curr_vert.co)
+#
+#             other_vert0 = curr_edges[0].other_vert(curr_vert)
+#             other_vert1 = curr_edges[1].other_vert(curr_vert)
+#             next_vert = other_vert0 if other_vert0 != prev_vert else other_vert1
+#
+#             prev_vert = curr_vert
+#             curr_vert = next_vert
+#             curr_edges = curr_vert.link_edges
+#
+#     if curr_vert != start_vert:
+#         pts.append(curr_vert.co)
+#         return False, pts
+#     else:
+#         return True, pts
 
 
 def _stencil_shape_to_stencil_mesh(from_vp_matrix, shape):
