4be8c7c788c51227faba3d851f33974b710696db
Allen Marshall <allen.marshall.22@gmail.com>
2019-03-13 19:31:26 -0500

    Initial implementation of Viewport Stencil Carve operator.

--- src/view_carve/stencil_op.py
+++ src/view_carve/stencil_op.py
@@ -1,4 +1,16 @@
+"""Contains the Viewport Stencil Carve operator."""
+
+import traceback
+
 import bpy
+
+from . import util_mesh
+from . import mesh_project
+
+
+# Threshold to use for finding overlapping geometry in boolean operations.
+# TODO: Make this configurable?
+_BOOLEAN_OP_THRESHOLD = 0.000001
 
 
 class VIEW_CARVE_OT_stencil(bpy.types.Operator):
@@ -27,3 +39,126 @@ class VIEW_CARVE_OT_stencil(bpy.types.Operator):
     bl_label = 'Viewport Stencil Carve'
     bl_description = 'Separate a mesh object based on other objects projected through the current view'
     bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        # Make sure we are in Object Mode with at least two selected objects.
+        if context is None \
+                or not hasattr(context, 'region_data') or context.region_data is None \
+                or not hasattr(context, 'scene') or context.scene is None \
+                or not hasattr(context.scene, 'objects') or context.scene.objects is None \
+                or not hasattr(context.scene.objects, 'active') or context.scene.objects.active is None \
+                or not hasattr(context, 'selected_objects') or context.selected_objects is None \
+                or len(list(context.selected_objects)) < 2 \
+                or not hasattr(context, 'mode') or context.mode != 'OBJECT' \
+                or context.scene.objects.active.type != 'MESH':
+            return False
+
+        # Make sure the carve target (active object) is a mesh object with no modifiers.
+        if context.scene.objects.active.type != 'MESH' or len(list(context.scene.objects.active.modifiers)) != 0:
+            return False
+
+        # TODO: Make sure all selected objects can be converted to meshes?
+
+        return True
+
+    def execute(self, context):
+        new_objs = []
+        stencil_mesh_objs = []
+        try:
+            # Find the carver objects.
+            orig_target = context.scene.objects.active
+            carver_objs = [obj for obj in list(context.selected_objects) if obj is not orig_target]
+
+            # Create stencil mesh objects from the carver objects. (Only one stencil mesh will be created if we are in
+            # Union Carves mode.)
+            stencil_mesh_objs = mesh_project.carvers_to_stencil_meshes(context.region_data.perspective_matrix,
+                                                                       carver_objs, self.prop_delete_carvers,
+                                                                       self.prop_union_carves, context)
+
+            # Apply each stencil mesh.
+            target_name = orig_target.name
+            targets = [orig_target]
+            for stencil_mesh_obj in stencil_mesh_objs:
+                new_targets = []
+                for target in targets:
+                    new_target = self._separate_obj(context, target, stencil_mesh_obj, target_name)
+                    if new_target is not None:
+                        new_targets.append(new_target)
+                        new_objs.append(new_target)
+                targets += new_targets
+
+            # Remove any objects that have been made empty by the boolean operations.
+            for target in targets:
+                if len(list(target.data.vertices)) == 0:
+                    if target is orig_target:
+                        orig_target = None
+                    bpy.data.objects.remove(target)
+                    new_objs.remove(target)
+
+            # Delete carver objects if we are in Delete Carver Objects mode.
+            if self.prop_delete_carvers:
+                for carver in carver_objs:
+                    bpy.data.objects.remove(carver)
+
+            # Set the final selection to something that makes sense for the operation.
+            bpy.ops.object.select_all(action='DESELECT')
+            for new_obj in new_objs:
+                new_obj.select = True
+            if orig_target is not None:
+                orig_target.select = True
+            context.scene.objects.active = orig_target
+
+            return {'FINISHED'}
+
+        except Exception as e:
+            # In case of error, try to clean up any partial results owned by Blender.
+            for new_obj in new_objs:
+                bpy.data.objects.remove(new_obj)
+
+            self.report({'ERROR'}, str(e))
+            traceback.print_exc()
+
+            return {'CANCELLED'}
+
+        finally:
+            # Clean up intermediate objects that were created, whether there was an error or not.
+            for stencil_mesh_obj in stencil_mesh_objs:
+                bpy.data.objects.remove(stencil_mesh_obj)
+
+    def _separate_obj(self, context, target, stencil_mesh_obj, base_target_name):
+        """Separates a mesh object using the specified stencil mesh.
+        Modifies the original target mesh using the stencil.
+        Returns the new mesh object obtained by cutting off a piece of the target, or None if no object was generated.
+        context - The Blender context.
+        target - The mesh object to carve.
+        stencil_mesh_obj - The mesh object to use for boolean operations on the target.
+        base_target_name - The base name to use for the new mesh object.
+        """
+        target_data_copy = None
+        new_target = None
+        try:
+            # If we are not in Subtract Only mode, we will need a copy of the target mesh data to create the new object.
+            if not self.prop_subtract_only:
+                target_data_copy = target.data.copy()
+
+            # Apply boolean modifier to the original target object.
+            util_mesh.apply_boolean_op(context, target, stencil_mesh_obj, 'DIFFERENCE', _BOOLEAN_OP_THRESHOLD)
+
+            # If we are not in Subtract Only mode, create a new object and apply the opposite boolean modifier.
+            if not self.prop_subtract_only:
+                new_target = bpy.data.objects.new(base_target_name, target_data_copy)
+                context.scene.objects.link(new_target)
+
+                util_mesh.apply_boolean_op(context, new_target, stencil_mesh_obj, 'INTERSECT', _BOOLEAN_OP_THRESHOLD)
+
+            return new_target
+
+        except Exception as e:
+            # Try to clean up.
+            if new_target is not None:
+                bpy.data.objects.remove(new_target)
+            if target_data_copy is not None:
+                bpy.data.meshes.remove(target_data_copy)
+
+            raise e
--- src/view_carve/util_mesh.py
+++ src/view_carve/util_mesh.py
@@ -1,57 +1,22 @@
+"""Utilities for working with meshes and other geometric objects."""
+
 import bpy
 
 
-def union_objects(context, objs, solver, double_threshold, delete=False, delete_data=False):
-    """Creates a new object containing the boolean union of the specified objects.
-    Returns: The new object
-    context - The Blender context
-    objs - Mesh objects to union
-    solver - Solver parameter for the boolean modifiers
-    double_threshold - Threshold parameter for the boolean modifiers
-    delete - If true, the objects in objs will be deleted after the union is performed.
-    delete_data - If true, the mesh data associated with the objects in objs will be deleted after the union is
-      performed."""
-    try:
-        # Start with an empty mesh object.
-        new_mesh = bpy.data.meshes.new('viewCarveTemp_mesh')
-        new_obj = bpy.data.objects.new('viewCarveTemp_meshObj', new_mesh)
-        context.scene.objects.link(new_obj)
-
-        for obj in objs:
-            apply_boolean_op(context, new_obj, obj, 'UNION', solver, double_threshold)
-
-        if delete:
-            for obj in objs:
-                data = obj.data if hasattr(obj, 'data') else None
-                bpy.data.objects.remove(obj)
-                if delete_data and data is not None:
-                    bpy.data.meshes.remove(data)
-
-        return new_obj
-
-    except Exception as e:
-        # Try to clean up.
-        if 'new_obj' in locals():
-            bpy.data.objects.remove(new_obj)
-        if 'new_mesh' in locals():
-            bpy.data.meshes.remove(new_mesh)
-
-        raise e
-
-
-def apply_boolean_op(context, target, obj, operation, solver, double_threshold):
+def apply_boolean_op(context, target, obj, operation, double_threshold):
     """Adds and immediately applies a boolean modifier to the specified target object.
+    Warning: This function may change the selection state of objects in the scene.
     May not work correctly if the target has unapplied modifiers.
-    context - The Blender context
-    target - The object to which the boolean modifier will be applied
-    obj - The object to use in the modifier
-    operation - Operation parameter for the boolean modifier
-    solver - Solver parameter for the boolean modifier
-    double_threshold - Threshold parameter for the boolean modifier"""
-    context.scene.objects.active = target
-    mod = target.modifiers.new('curveSepTemp_modifier', 'BOOLEAN')
+    Raises ValueError if Blender is not currently in Object Mode.
+    context - The Blender context.
+    target - The object to which the boolean modifier will be applied.
+    obj - The object to use in the modifier.
+    operation - Operation parameter for the boolean modifier.
+    double_threshold - Threshold parameter for the boolean modifier.
+    """
+    mod = target.modifiers.new('viewCarveTemp_modifier', 'BOOLEAN')
     mod.operation = operation
     mod.object = obj
-    mod.solver = solver
     mod.double_threshold = double_threshold
+    context.scene.objects.active = target
     bpy.ops.object.modifier_apply(modifier=mod.name)
