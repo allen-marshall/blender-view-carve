8e0b4f4c565f7c037f8b8a06a2a172588120a94f
Allen Marshall <allen.marshall.22@gmail.com>
2019-05-17 00:15:10 -0500

    Updated version number to 1.0.0.

--- src/view_carve/__init__.py
+++ src/view_carve/__init__.py
@@ -9,7 +9,7 @@ bl_info = {
     'name': 'View Carve',
     'description': 'Tool for carving a mesh object based on projections of other objects in the 3D viewport',
     'author': 'Allen Marshall',
-    'version': (0, 4),
+    'version': (1, 0, 0),
     'blender': (2, 80, 0),
     'category': 'Mesh',
     'tracker_url': 'https://github.com/allen-marshall/blender-view-carve/issues',
